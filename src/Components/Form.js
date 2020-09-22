911e70fee80b0053569ba33fcc3740d2c6e22def
Julius-R <julius.robinson95@yahoo.com>
2020-09-22 15:29:54 -0500

    Completed form and ui

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,49 +1,49 @@
 import React, {useState} from 'react'
-
-
-const formFields = ['name', 'email', 'date']
+import { useForm } from "react-hook-form";
+import axios from 'axios';
 
 export default function Form() {
-    const [name, setName] = useState(null)
-    const [email, setEmail] = useState(null)
-    const [date, setDate] = useState(null)
-
-    const submitForm = (e) => {
-        e.preventDefault()
-        console.log(name, email, date)
+    const [isOpen, setisOpen] = useState(false)
+    const { register, errors, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post(`https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users`, { ...data })
+      .then(res => {
+        setisOpen(true)
+        reset();
+        setTimeout(() => setisOpen(false), 2000)
+      })
     }
-    const checkBlank = (field) => {
 
-    };
-    const updateField = (field) => {
-        const val = event.target.value
-        switch(field) {
-            case "name":
-                setName(val)
-                break;
-            case "email":
-                setEmail(val)
-                break;
-            case "date":
-                setDate(val)
-                break;
-            default:
-                break;
-        }
-    }
     return (
         <section className="contact">
-            <form action="POST" className="form" onSubmit={submitForm}>
-                {formFields.map(field => {
-                    return (
-                        <section className="form__area">
-                        <label htmlFor={field}>{field}</label>
-                        <input type={field} onChange={() => updateField(field)} />
-                        {field != "date" ? <p className="error">This field can not be left blank.</p> : null}
-                        </section>
-                    )
-                })}
-                <button>Submit</button>
+            <section className="notification" id={isOpen && "slide-bottom"}>
+                <p>Successfully Submitted!</p>
+            </section>
+            <form action="POST" className="form" onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="title">Contact Us</h1>
+                <section className="form__area">
+                    <label htmlFor="name">Name</label>
+                    <input name="name" ref={register({required: true})} />
+                    {errors.name && <p className="error">This field can not be left blank.</p>}
+                </section>
+                <section className="form__area">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" ref={register({required: true})} />
+                    {errors.email && <p className="error">This field can not be left blank.</p>}
+                </section>
+                <section className="form__area">
+                    <label htmlFor="birthDate">Birth Date</label>
+                    <input type="date" name="birthDate" ref={register} />
+                </section>
+                <section className="form__area check"> 
+                    <input type="checkbox" name="emailConsent" ref={register} />
+                    <p>I agree to be contacted via email.</p>
+                </section>
+                <section className="form__area btns"> 
+                    <input id="clear" type="reset" value="Clear" />
+                    <input id="submit" type="submit" value="Submit" />
+                </section>
             </form>
         </section>
     )
